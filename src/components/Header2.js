3890c26414121b01220cd128ec791846d0af6acc
Hritik Sharma <2002newhritik@gmail.com>
2021-06-20 13:08:28 +0530

    Added /About to navlink about

--- src/components/Header2.js
+++ src/components/Header2.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Header2 = ()=>{
     return(
         <div className="Header2">
-            <NavLink exact activeClassName="active" to="/">ABOUT ME</NavLink>
+            <NavLink exact activeClassName="active" to="/About">ABOUT ME</NavLink>
             <NavLink exact activeClassName="active" to="/Project">PROJECTS</NavLink>
             <NavLink exact activeClassName="active" to="/Resume">RESUME</NavLink>
             <NavLink exact activeClassName="active" to="/Contact">CONTACT</NavLink>
